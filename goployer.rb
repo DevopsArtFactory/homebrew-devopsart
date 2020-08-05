cb3613aead991f5a1bf6ac09b91570dbb6f1843d
Gwonsoo Lee <gslee@bighitcorp.com>
2020-08-06 03:58:32 +0900

    release 2.0.1

--- goployer.rb
+++ goployer.rb
@@ -4,8 +4,8 @@
 class Goployer < Formula
   desc "AWS Deployer with Go"
   homepage "https://goployer.dev"
-  url "https://github.com/DevopsArtFactory/goployer/releases/download/2.0.0/goployer-darwin-amd64"
-  sha256 "d25e7e99c3ca519b562f44f322d32836590d52d8807a680ea4ffd612ce355689"
+    url "https://github.com/DevopsArtFactory/goployer/releases/download/2.0.1/goployer-darwin-amd64"
+  sha256 "01c794e77f25130352cb8b5fb39bf4b0782f23612fd5eea2847ac97b785203ff"
   license "Apache-2.0"
 
   def install
