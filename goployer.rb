b3d76fe5ac9d4c1f140e00ecdc4aae00d70a412b
Gwonsoo Lee <gslee@bighitcorp.com>
2020-08-06 18:15:56 +0900

    fix

--- goployer.rb
+++ goployer.rb
@@ -5,7 +5,7 @@ class Goployer < Formula
   desc "AWS Deployer with Go"
   homepage "https://goployer.dev"
   url "https://github.com/DevopsArtFactory/goployer/releases/download/2.0.2/goployer"
-  sha256 "aaa13539e73c2e389a00a9062c67f79513e3c895cc4299a6635b03a410330746"
+  sha256 "1f9cd30839ecb9244a43b33cec73c82766e99188d4d7401638c99021de64ab08"
   license "Apache-2.0"
 
   def install
