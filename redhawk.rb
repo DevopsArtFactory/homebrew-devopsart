b9ffdefcb185ccc709c80507c5f14367f0724bbb
GwonsooLee <gslee@bighitcorp.com>
2020-10-30 11:43:13 +0900

    release hotfix 0.0.2

--- redhawk.rb
+++ redhawk.rb
@@ -4,8 +4,8 @@
 class Redhawk < Formula
   desc "Open source infrastructure audit and management tool."
   homepage ""
-  url "https://github.com/DevopsArtFactory/redhawk/releases/download/0.0.1/redhawk"
-  sha256 "985f53dcd5bafa9ff530982e63109462568e94cc19194dc40a398721a9686099"
+  url "https://github.com/DevopsArtFactory/redhawk/releases/download/0.0.2/redhawk"
+  sha256 "35efb374eb744d4e4d4e1f901d2781b568b20b1f9b4ce81948a635239606f04f"
   license "Apache-2.0"
 
   # depends_on "cmake" => :build
