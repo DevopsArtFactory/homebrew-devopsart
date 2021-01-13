16ee152ce672d0c27073fa04e432b403b8ba3d15
GwonsooLee <gslee@bighitcorp.com>
2021-01-13 18:37:40 +0900

    add escli

---
+++ escli.rb
@@ -0,0 +1,16 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Redhawk < Formula
+  desc "Open source elasticsearch CLI"
+  homepage ""
+  url "https://github.com/DevopsArtFactory/escli/releases/download/0.0.1/escli"
+  sha256 "2d1f9e291516ff749d0cd8d755b9b836d25144b1fa0f874bd0904498321fb767"
+  license "Apache-2.0"
+
+  # depends_on "cmake" => :build
+
+  def install
+    bin.install "escli"
+  end
+end
